0b12246874cda45f48db03360c42140aba4f6882
TatianaJSM <tatiana.jimenezsolis@ucr.ac.cr>
2026-06-26 14:41:28 -0600

    Agregar clase de Material

---
+++ app/Models/Material.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Material extends Model
+{
+    protected $table = 'materiales';
+
+    protected $primaryKey = 'codigo';
+
+    protected $fillable = ['unidadMedida', 'descripcion', 'ubicacion', 'categoria_fk'];
+
+   
+    public function categoria(): BelongsTo
+    {
+        return $this->belongsTo(Categoria::class, 'categoria_fk', 'idCategoria');
+    }
+
+    public function materialUnidades(): HasMany
+    {
+        return $this->hasMany(MaterialUnidad::class, 'material_fk', 'codigo');
+    }
+}
