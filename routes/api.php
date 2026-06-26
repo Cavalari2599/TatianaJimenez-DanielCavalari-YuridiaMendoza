18a00d4851636ae0b31bf8a89acc358cc343a730
TatianaJSM <tatiana.jimenezsolis@ucr.ac.cr>
2026-06-26 15:09:04 -0600

    rutas

--- routes/api.php
+++ routes/api.php
@@ -5,3 +5,4 @@ use Illuminate\Support\Facades\Route;
 
 // Endpoints SOLINVORD - Material (Parte II.B)
 Route::put('/materiales/{codigo}', [MaterialController::class, 'update']); // equipo 2: actualizar material
+Route::post('/materiales', [MaterialController::class, 'store']);          // equipo 1: insertar material + categoria
